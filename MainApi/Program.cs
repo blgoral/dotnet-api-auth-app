8750c07cb464d1363dd0c0765bd19953b6b551ab
Blake Goral <blakegoral@gmail.com>
2022-08-25 23:36:51 -0500

    configure jwt authentication

--- MainApi/Program.cs
+++ MainApi/Program.cs
@@ -1,9 +1,31 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services
+    .AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.MetadataAddress = builder.Configuration["Jwt:MetaDataAddress"]!;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidAudience = builder.Configuration["Jwt:ValidAudience"]
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.DefaultPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
